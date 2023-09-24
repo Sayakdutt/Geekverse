bf370f396d1e7a457a0c9b4a950ba2a7da9b7c6e
Sayakdutt <sayakofficial01@gmail.com>
2023-09-24 17:35:27 +0530

    Updated index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import LoginImg from "./LoginImg.svg";
 import Frame from "./Frame.svg";
+import {NoProfile} from "./userprofile.png";
 import F from "./F.svg";
-export { LoginImg, Frame, F };
+export { LoginImg, Frame, F , NoProfile }
